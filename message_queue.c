707642c8406b10db1349ced4e590e2a50c219a2e
subhashree <subhasubha22112005@gmail.com>
2026-06-24 07:13:51 +0530

    Question 10 Message Queue

---
+++ message_queue.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#include <string.h>
+
+struct msgbuf
+{
+    long mtype;
+    char mtext[100];
+};
+
+int main()
+{
+    int msgid;
+    struct msgbuf message;
+
+    msgid = msgget(1234, 0666 | IPC_CREAT);
+
+    message.mtype = 1;
+    strcpy(message.mtext, "Hello Queue");
+
+    msgsnd(msgid, &message, sizeof(message.mtext), 0);
+
+    msgrcv(msgid, &message, sizeof(message.mtext), 1, 0);
+
+    printf("Received: %s\n", message.mtext);
+
+    return 0;
+}
